3e0848c806481b8228cc01719af36f7e6dc482f5
Marcin Butora <36995841+marcinbutora@users.noreply.github.com>
2022-12-06 11:24:15 +0100

    sample data and model

--- src/mock-data/words.ts
+++ src/mock-data/words.ts
@@ -1,6 +1,6 @@
 export const words = [
   { eng: "smattering of", pol: ["powierzchniowo"]},
-  { eng: "bear with me", pol: [["proszę poczekać"], ["proszę o chwilę cierpliwości"]]},
-  { eng: "by all means", pol: [["oczywiście"], ["jak najbardziej"]]},
-  { eng: "oczywiście, jak najbardziej", pol: [["na dziś koniec"], ["wystarczy na dziś"], ["zakończyć pracę"]]},
+  { eng: "bear with me", pol: ["proszę poczekać", "proszę o chwilę cierpliwości"]},
+  { eng: "by all means", pol: ["oczywiście", "jak najbardziej"]},
+  { eng: "call it a day", pol: ["na dziś koniec", "wystarczy na dziś", "zakończyć pracę"]},
 ]
---
+++ src/model/word.ts
@@ -0,0 +1,5 @@
+export interface Word {
+  eng: string;
+  pol: string[];
+}
+
